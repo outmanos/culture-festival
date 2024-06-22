ac6903ce88f5fe88be68d629d6bc8cc9b8543f59
Outman <23385633+outmanos@users.noreply.github.com>
2024-06-22 19:45:20 +0200

    testing sass

--- cfms-ui/app/page.tsx
+++ cfms-ui/app/page.tsx
@@ -1,9 +1,9 @@
-import styles from "@/styles/variables.module.scss"; // Import your styles
+import styles from "./variables.module.scss";
 
 export default function Home() {
   return (
       <main>
-        <h1>Using Sass in Next.js</h1>
+        <h1 style={{ color: styles.secondaryColor }}>Using Sass in Next.js</h1>
         <p style={{ color: styles.primaryColor }}>
           This text uses the primary color defined in Sass.
         </p>
